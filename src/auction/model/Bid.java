636b6bb50390d1251b07676109a2133776b9ca6a
Hieu <hieuunq12@gmail.com>
2026-03-30 14:12:54 +0700

    add Bid model class

---
+++ src/auction/model/Bid.java
@@ -0,0 +1,19 @@
+package auction.model;
+
+public class Bid {
+    private String bidder;
+    private double amount;
+
+    public Bid(String bidder, double amount){
+        this.bidder = bidder;
+        this.amount = amount;
+    }
+
+    public String getBidder(){
+        return bidder;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+}
