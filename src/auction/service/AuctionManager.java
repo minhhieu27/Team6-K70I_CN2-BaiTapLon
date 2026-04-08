1bb7f770281ec988474923f45edb1642ce30dae6
Hieu <hieuunq12@gmail.com>
2026-04-08 14:26:42 +0700

    update

--- src/auction/service/AuctionManager.java
+++ src/auction/service/AuctionManager.java
@@ -6,7 +6,18 @@ import java.util.*;
 
 public class AuctionManager {
     
+    private static AuctionManager instance;
+
     private Map<String, Auction> map = new HashMap<>(); // Tạo bảng để lưu key-value cho các phiên đấu giá
+
+    private AuctionManager(){}
+
+    public static AuctionManager getInstance(){
+        if (instance == null){
+            instance = new AuctionManager();
+        }
+        return instance;
+    }
 
     public void addAuction(Auction auction){ // Thêm phiên đấu giá
         map.put(auction.getItemName(), auction);
@@ -14,5 +25,10 @@ public class AuctionManager {
 
     public Auction getAuction(String item){ // Lấy phiên đấu giá
         return map.get(item); 
+    }
+
+    // Lấy tất cả Auction
+    public Collection<Auction> getAllAuction(){
+        return map.values();
     }
 }
