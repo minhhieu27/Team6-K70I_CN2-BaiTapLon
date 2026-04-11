5273d7197ea0a782913f4e2c7a43e40d0d580ed7
Hieu <hieuunq12@gmail.com>
2026-04-12 05:57:18 +0700

    update

--- src/auction/model/Auction.java
+++ src/auction/model/Auction.java
@@ -1,6 +1,11 @@
 package auction.model;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+import auction.observer.Observer;
+
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Auction {
@@ -10,8 +15,9 @@ public class Auction {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private AuctionStatus status;
-
     private List<Bid> bidHistory;
+    private List<Observer> observers = new ArrayList<>();
+    private ReentrantLock lock = new ReentrantLock();
 
     public Auction(String itemName, double startPrice){
         this.itemName = itemName;
@@ -25,7 +31,21 @@ public class Auction {
         this.bidHistory = new ArrayList<>();
     }
 
-    public void updateStatus(){
+    public void addObserver(Observer o){ // Hàm đăng ký xem đấu giá
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o){ // Hàm hủy đăng ký xem đấu giá
+        observers.remove(o);
+    }
+
+    public void notifyObservers(String message){ // Thông báo 
+        for (Observer o : observers){
+            o.update(message);
+        }
+    }
+
+    public void updateStatus(){ // Update trạng thái của phiên đấu giá
         LocalDateTime now = LocalDateTime.now();
 
         if (status == AuctionStatus.CANCELLED || status == AuctionStatus.PAID){
@@ -50,40 +70,59 @@ public class Auction {
         return status;
     }
 
-    public void paidItem(){
+    public void paidItem(){ // Nếu đấu giá thành công thì hiện đã bán
         if (status == AuctionStatus.FINISHED){
             status = AuctionStatus.PAID;
         }
     }
 
-    public void cancelledItem(){
+    public void cancelledItem(){ // Hủy đấu giá
         if (status == AuctionStatus.SCHEDULED || status == AuctionStatus.OPEN){
             status = AuctionStatus.CANCELLED;
         }
+    }
+
+    public void extendAuctionTime(){ // Gia hạn thời gian
+
+        long secondsLeft = Duration.between(LocalDateTime.now(), endTime).getSeconds();
+
+        if (secondsLeft <= 30){
+            endTime = endTime.plusSeconds(60);
+        }
+        System.out.println("Auction extended 60 seconds!");
     }
        
     public double getMiniumBid(){ // Giá đấu ít nhất phải bằng 1.1 giá hiện tại 
         return getCurrentPrice() * 1.1;
     }
 
-    public boolean placeBid(Bid bid){
+    public boolean placeBid(Bid bid){ // Đấu giá
 
-        updateStatus();
+        lock.lock();
+        try {
+            updateStatus();
+            
+            if (status != AuctionStatus.OPEN){
+                System.out.println("Auction is not open");
+                return false;
+    
+            }
+            
+            if (bid.getAmount() <= getMiniumBid()){
+                return false;
+            }
+    
+            bidHistory.add(bid);
+    
+            extendAuctionTime();
+            
+            notifyObservers("New bid: " + bid.getAmount() + " by " + bid.getBidder());
 
-        if (status != AuctionStatus.OPEN){
-            System.out.println("Auction is not open");
-            return false;
-
+            return true;
+        } finally {
+            lock.unlock();
         }
-        if (bid.getAmount() <= getMiniumBid()){
-            return false;
-        }
 
-        bidHistory.add(bid);
-
-        System.out.println("New highest bidder: " + bid.getBidder() + "| Price: " + bid.getAmount());
-
-        return true;
     }
 
     public String getItemName(){
