3674fde75da0ae85fc288cf3fc954fb10eaa0395
Hieu <hieuunq12@gmail.com>
2026-04-22 14:05:41 +0700

    Sửa lỗi

--- src/auction/mapper/AuctionMapper.java
+++ src/auction/mapper/AuctionMapper.java
@@ -8,12 +8,12 @@ import java.util.*;
 public class AuctionMapper {
 
     public static AuctionView toView(Auction auction){
-       String highestBidder = auction.getHighestBidder();
+        String highestBidder = auction.getHighestBidder();
 
-       if (highestBidder == null){
-        highestBidder = "None";
-       }
-       Money currentPrice = auction.getCurrentPrice();
+        if (highestBidder == null){
+            highestBidder = "None";
+        }
+        Money currentPrice = auction.getCurrentPrice();
 
         return new AuctionView(auction.getItemName(), currentPrice, highestBidder);
     }
