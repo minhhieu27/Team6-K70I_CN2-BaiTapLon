aa47d4e1401e4dad4ef1eeb631ea51eb6f4e8663
Hieu <hieuunq12@gmail.com>
2026-04-28 22:25:03 +0700

    Fix VIPLevel

--- src/main/java/users/enums/VIPLevel.java
+++ src/main/java/users/enums/VIPLevel.java
@@ -1,9 +1,22 @@
 package users.enums;
 
+import java.math.BigDecimal;
+import auction.model.Money;;
+
 public enum VIPLevel {
-    NORMAL,
-    BRONZE,
-    SILVER,
-    GOLD,
-    DIAMOND;
+    NORMAL(new Money(BigDecimal.ZERO)),
+    BRONZE(new Money(new BigDecimal("1000"))),
+    SILVER(new Money(new BigDecimal("5000"))),
+    GOLD(new Money(new BigDecimal("10000)"))),
+    DIAMOND(new Money(new BigDecimal("50000")));
+
+    private final Money requiredWithdraw;
+    
+    VIPLevel(Money requiredWithdraw){
+        this.requiredWithdraw = requiredWithdraw;
+    }
+
+    public Money getRequiredWithdraw(){
+        return requiredWithdraw;
+    }
 }
