b81f0fe49de05f2ef78419c477d944ba700a06f3
Hieu <hieuunq12@gmail.com>
2026-04-28 22:04:18 +0700

    Chỉnh sửa phương thức upgrade VIP

--- src/main/java/users/enums/VIPLevel.java
+++ src/main/java/users/enums/VIPLevel.java
@@ -1,21 +1,9 @@
 package users.enums;
 
-import auction.model.Money;
-
 public enum VIPLevel {
-    NORMAL(new Money(0.0)),
-    BRONZE(new Money(100.0)),
-    SILVER(new Money(1000.0)),
-    GOLD(new Money(5000.0)),
-    DIAMOND(new Money(10000.0));
-
-    private final Money requiredDeposit;
-
-    VIPLevel(Money requiredDeposit){
-        this.requiredDeposit = requiredDeposit;
-    }
-
-    public Money getRequiredDeposit(){
-        return requiredDeposit;
-    }
+    NORMAL,
+    BRONZE,
+    SILVER,
+    GOLD,
+    DIAMOND;
 }
